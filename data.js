1039d50f2c2e8a5cd01666f236adccfbe5d41e66
yosefus <yosalsoni@gmail.com>
2023-06-04 10:31:14 +0300

    first

---
+++ data.js
@@ -0,0 +1,105 @@
+let flights = [
+   {
+      flightNumber: 'AA123',
+      timeDepart: '08:00',
+      timeArrive: '10:00',
+      cityDepart: 'New York',
+      cityArrive: 'Chicago',
+      gate: 'A1',
+      id: 1
+   },
+   {
+      flightNumber: 'BA456',
+      timeDepart: '09:00',
+      timeArrive: '11:00',
+      cityDepart: 'London',
+      cityArrive: 'Paris',
+      gate: 'B2',
+      id: 2
+   },
+   {
+      flightNumber: 'CA789',
+      timeDepart: '10:00',
+      timeArrive: '12:00',
+      cityDepart: 'Beijing',
+      cityArrive: 'Tokyo',
+      gate: 'C3',
+      id: 3
+   },
+   {
+      flightNumber: 'DA123',
+      timeDepart: '11:00',
+      timeArrive: '13:00',
+      cityDepart: 'Los Angeles',
+      cityArrive: 'San Francisco',
+      gate: 'D1',
+      id: 4
+   },
+   {
+      flightNumber: 'EA456',
+      timeDepart: '12:00',
+      timeArrive: '14:00',
+      cityDepart: 'Berlin',
+      cityArrive: 'Rome',
+      gate: 'E2',
+      id: 5
+   },
+   {
+      flightNumber: 'FA789',
+      timeDepart: '13:00',
+      timeArrive: '15:00',
+      cityDepart: 'Moscow',
+      cityArrive: 'Dubai',
+      gate: 'F3',
+      id: 6
+   },
+   {
+      flightNumber: 'GA123',
+      timeDepart: '14:00',
+      timeArrive: '16:00',
+      cityDepart: 'Sydney',
+      cityArrive: 'Melbourne',
+      gate: 'G1',
+      id: 7
+   },
+   {
+      flightNumber: 'HA456',
+      timeDepart: '15:00',
+      timeArrive: '17:00',
+      cityDepart: 'New Delhi',
+      cityArrive: 'Mumbai',
+      gate: 'H2',
+      id: 8
+   },
+   {
+      flightNumber: 'IA789',
+      timeDepart: '16:00',
+      timeArrive: '18:00',
+      cityDepart: 'Rio de Janeiro',
+      cityArrive: 'Sao Paulo',
+      gate: 'I3',
+      id: 9
+   },
+   {
+      flightNumber: 'JA123',
+      timeDepart: '17:00',
+      timeArrive: '19:00',
+      cityDepart: 'Toronto',
+      cityArrive: 'Vancouver',
+      gate: 'J1',
+      id: 10
+   },
+   {
+      flightNumber: 'KA456',
+      timeDepart: '18:00',
+      timeArrive: '20:00',
+      cityDepart: 'Cape Town',
+      cityArrive: 'Johannesburg',
+      gate: 'K2',
+      id: 11
+   }
+];
+
+
+
+module.exports = { flights }
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(require('cors')())
+
+const { flights } = require('./data')
+
+app.post('/flights', (req, res) => {
+   const flight = req.body;
+
+   if (
+      !flight.flightNumber ||
+      !flight.timeDepart ||
+      !flight.timeArrive ||
+      !flight.cityDepart ||
+      !flight.cityArrive ||
+      !flight.gate
+   ) {
+      return res.status(400).json({ message: 'Missing required data' });
+   }
+
+   const maxId = Math.max(...flights.map((f) => f.id));
+   flight.id = maxId + 1;
+
+   flights.push(flight);
+
+   res.status(201).json(flight);
+});
+
+app.put('/flights/:id', (req, res) => {
+   const id = parseInt(req.params.id);
+   const flight = flights.find((f) => f.id === id);
+   if (!flight) return res.status(404).json({ message: 'Flight not found' });
+   const updatedData = req.body;
+   Object.assign(flight, updatedData);
+   res.json(flight);
+});
+
+app.get('/flights/:id', (req, res) => {
+   const id = parseInt(req.params.id);
+   const flight = flights.find((f) => f.id === id);
+   if (!flight) return res.status(404).json({ message: 'Flight not found' });
+   res.json(flight);
+});
+
+app.get('/flights', (req, res) => {
+   res.json(flights);
+});
+
+app.delete('/flights/:id', (req, res) => {
+   const id = parseInt(req.params.id);
+   const index = flights.findIndex((f) => f.id === id);
+   if (index === -1) return res.status(404).json({ message: 'Flight not found' });
+   flights.splice(index, 1);
+   res.json({ message: 'Flight deleted' });
+});
+
+app.listen(4001, () => console.log('Server running on port 4001'));
